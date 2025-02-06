79bfa8afba03d34de1787e2395977fa3e45a6377
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-02-06 15:51:52 +0000

    Landing Page con React David Vaamonde

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -8,12 +8,13 @@ const Navbar = () => {
                 <div class="container">
                     
                     <a class="navbar-brand" href="#">Start BootStrap</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSoportado" aria-controls="navbarSoportado" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSoportado">
-                        <ul class="navbar-nav">
+                    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+                        
+                        <ul class="navbar-nav d-flex justify-content-end">
                             <li class="nav-item active">
                                 <a class="nav-link" href="#">Home </a>
                             </li>
